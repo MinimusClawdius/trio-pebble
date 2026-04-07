b601e1f07db567fd964de576176b5f09addc2189
jteene <jteene@questrade.com>
2026-04-07 17:01:51 -0400

    v2.4.2: fix CloudPebble duplicate IMAGE_MENU_ICON (remove resources from docs manifest)

--- src/pkjs/index.js
+++ src/pkjs/index.js
@@ -621,7 +621,7 @@ Pebble.addEventListener('appmessage', function (e) {
 
 // ---------- Ready ----------
 Pebble.addEventListener('ready', function () {
-    console.log('Trio Pebble v2.4.1 ready');
+    console.log('Trio Pebble v2.4.2 ready');
     loadSettings();
 
     var msg = {};
